aac3eb93a576b8d22798dd385b8a4630ee1cd7dd
Sara Alghamdi <SaraSaeed.gh@gmail.com>
2022-05-26 23:05:33 +0300

    some edit

--- week2/Assignment/ArrayChallenges.js
+++ week2/Assignment/ArrayChallenges.js
@@ -1,7 +1,19 @@
+/* Always Hungry
+Write a function that is given an array and each time the value is "food" it should console log "yummy". If "food" was not present in the array console log "I'm hungry" once.
+High Pass Filter
+Given an array and a value cutoff, return a new array containing only the values larger than cutoff.
+Better than average
+Given an array of numbers return a count of how many of the numbers are larger than the average.
+Array Reverse
+Write a function that will reverse the values an array and return them.
+Fibonacci Array
+Fibonacci numbers have been studied for years and appear often in nature. Write a function that will return an array of Fibonacci numbers up to a given length n. Fibonacci numbers are calculated by adding the last two values in the sequence together. So if the 4th value is 2 and the 5th value is 3 then the next value in the sequence is 5.
+*/ 
+
+
 // Always Hungry
 arr1= [3.14, "food", "pie", true, "food"];
 arr2= [4, 1, 5, 7, 2];
-
 function alwaysHungry(arr) {
     var out;            
     for(var i=0 ; i<arr.length ; i++){
@@ -29,10 +41,10 @@ function highPass(arr, cutoff) {
         }
     }return filteredArr;
 }
-
 console.log("High Pass Filter")
 var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
 console.log(result); 
+
 
 //Better than average
 function betterThanAverage(arr) {
@@ -51,7 +63,6 @@ function betterThanAverage(arr) {
     // console.log(sum)
     return count;
 }
-
 console.log("Better than average");
 var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
 console.log(result); // we expect back 4
@@ -64,7 +75,6 @@ function reverse(arr) {
         newarr.push(arr[i]);
     }return newarr;
 }
-
 console.log("Array Reverse");
 var result = reverse(["a", "b", "c", "d", "e"]);
 console.log(result); // we expect back ["e", "d", "c", "b", "a"]
@@ -74,8 +84,14 @@ console.log(result); // we expect back ["e", "d", "c", "b", "a"]
 function fibonacciArray(n) {
     // the [0, 1] are the starting values of the array to calculate the rest from
     var fibArr = [0, 1];
-    // your code here
-    return fibArr;
+    while (n > 0){
+        temp = fibArr[0];
+        fibArr[0] = fibArr[0] + fibArr[1];
+        fibArr[1] = temp;
+        n--;
+        console.log(temp)
+        }
+    return temp;
 }
     
 var result = fibonacciArray(10);
