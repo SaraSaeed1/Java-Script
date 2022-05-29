4f3bb4f2332c23d086ad378150a226b9a5cfd160
Sara Alghamdi <SaraSaeed.gh@gmail.com>
2022-05-29 10:11:38 +0300

    New assignment

---
+++ week2/In Class/test.js
@@ -0,0 +1,13 @@
+function example() {
+    for (i=0 ; i<=5 ; i++){
+        console.log(i);
+    }
+    console.log("the end of loop"+ i);
+}
+
+
+function turnOff(element) {
+        element.remove();
+}
+
+
