bfd053c2304936df583906eddc16573e0641fcd3
Sara Alghamdi <SaraSaeed.gh@gmail.com>
2022-05-30 19:36:42 +0300

    add new function

--- week2/Assignment/Liking Likes/Part2/script.js
+++ week2/Assignment/Liking Likes/Part2/script.js
@@ -1,26 +1,6 @@
-var x = document.querySelector("#like1");
-var y = document.querySelector("#like2");
-var z = document.querySelector("#like3");
-var count1 =0;
-var count2 =0;
-var count3 =0;
 
-
-console.log(x);
-function add1(){
-    count1++;
-    x.innerText= " like(s) "+ count1;
-    console.log(count1);
-}
-
-function add2(){
-    count2++;
-    y.innerText= " like(s) "+ count2;
-    console.log(count2);
-}
-
-function add3(){
-    count3++;
-    z.innerText= " like(s) "+ count3;
-    console.log(count3);
+function add(e){
+    var likes =e.parentElement.querySelector("p #likes");
+    let count=parseInt(likes.innerText);
+    likes.innerText= count+1;
 }
